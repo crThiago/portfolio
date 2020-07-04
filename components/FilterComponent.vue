0a1b52275b65f8578c17b82679628b71b2da0ac5
Thiago Costa <thiago_8777@hotmail.com>
2020-07-04 19:52:40 -0300

    Animacao do filtro

--- components/FilterComponent.vue
+++ components/FilterComponent.vue
@@ -1,9 +1,18 @@
 <template>
   <div class="filter">
-    <div class="justify--center">
-      <a href=""><img class="tech__image" src="~/assets/images/laravel.svg" alt=""></a>
-      <a href=""><img class="tech__image" src="~/assets/images/php.svg" alt=""></a>
-      <a href=""><img class="tech__image" src="~/assets/images/graphql.svg" alt=""></a>
+    <div class="filter__techs justify--center">
+      <a class="tech" href="">
+        <img class="tech__image" src="~/assets/images/laravel.svg" alt="">
+        <span class="tech__name color--white font--bold">Laravel</span>
+      </a>
+      <a class="tech" href="">
+        <img class="tech__image" src="~/assets/images/php.svg" alt="">
+        <span class="tech__name color--white font--bold">PHP</span>
+      </a>
+      <a class="tech" href="">
+        <img class="tech__image" src="~/assets/images/graphql.svg" alt="">
+        <span class="tech__name color--white font--bold">GraphQL</span>
+      </a>
     </div>
   </div>
 </template>
@@ -12,18 +21,40 @@
 .filter {
   max-width: 320px;
   margin: 60px auto;
-  background: url('~assets/images/filter-bar.svg') bottom no-repeat;
-  background-size: contain;
-}
 
-.tech__image {
-  width: 35px;
+  &__techs {
+    height: 100px;
+    background: url('~assets/images/filter-bar.svg') bottom no-repeat;
+    background-size: contain;
+    align-items: end;
+  }
+}
+.tech {
   padding: 0 10px 15px;
   transition: all .3s;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  &__image {
+    width: 35px;
+  }
+
+  &__name {
+    height: 0px;
+    display: none;
+    font-size: 0.7em;
+  }
 
   &:hover {
-    scale: 1.5;
     padding: 0 20px 15px;
+    margin-bottom: 15px;
+    scale: 1.5;
+
+    .tech__name {
+      display: block;
+    }
   }
 }
 </style>
