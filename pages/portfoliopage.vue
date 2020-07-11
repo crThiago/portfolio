70c9fc8d388e416f5799b53dfb2991feabb04b4d
Thiago Costa <thiago_8777@hotmail.com>
2020-07-11 11:30:17 -0300

    Todos os projetos

--- pages/portfoliopage.vue
+++ pages/portfoliopage.vue
@@ -9,11 +9,7 @@
         <FilterComponent />
       </div>
       <div class="portfolio__cards justify--space-around flex--wrap">
-        <CardPortfolio />
-        <CardPortfolio />
-        <CardPortfolio />
-        <CardPortfolio />
-        <CardPortfolio />
+        <CardPortfolio v-for="project of projects" :key="project.slug" :project="project" />
       </div>
     </main>
   </div>
@@ -23,7 +19,114 @@
 export default {
   data () {
     return {
-      title: 'Portfólio'
+      title: 'Portfólio',
+      projects: [
+        {
+          slug: 'sistema-jueri',
+          name: 'Sistema Jueri',
+          resume: 'Novas Implementações e Criação da versão 3.0',
+          link: 'https://www.jueri.com.br',
+          company: {
+            name: 'Mídia Interativa',
+            link: 'https://www.midiainterativa.com.br'
+          },
+          date: {
+            start: '2017',
+            end: 'Atual'
+          },
+          techs: ['css', 'jquery', 'php']
+        },
+        {
+          slug: 'ecommerce-esinstrumentos',
+          name: 'ES Instrumentos Musicais',
+          resume: 'Criação de e-commerce',
+          link: 'https://www.esinstrumentosmusicais.com.br/',
+          company: {
+            name: 'Mídia Interativa',
+            link: 'https://www.midiainterativa.com.br'
+          },
+          date: {
+            start: '',
+            end: ''
+          },
+          techs: ['bootstrap', 'workcontrol', 'rest']
+        },
+        {
+          slug: 'sistema-uninova',
+          name: 'Uninova Saúde',
+          resume: 'Criação de sistema de cadastro, agendamento de consultas, histórico de consultas, controle de financeiro e mais...',
+          link: 'https://uninovasaude.com.br/',
+          company: {
+            name: 'Rem Soft',
+            link: 'https://www.remsoft.com.br/'
+          },
+          date: {
+            start: '',
+            end: ''
+          },
+          techs: ['php', 'css', 'jquery']
+        },
+        {
+          slug: 'sistema-petra',
+          name: 'Petra Gestão',
+          resume: 'Criação de sistema de workflow para contratação de novos colaboradores para as empresas parceiras',
+          link: 'https://www.petracorporate.com.br/',
+          company: {
+            name: '',
+            link: ''
+          },
+          date: {
+            start: '',
+            end: ''
+          },
+          techs: ['php', 'css', 'jquery']
+        },
+        {
+          slug: 'sistema-sintras',
+          name: 'Sintras',
+          resume: 'Novas implementações no sistema de agendamento de consultas',
+          link: 'http://www.sintras.net/',
+          company: {
+            name: 'Mídia Interativa',
+            link: 'https://www.midiainterativa.com.br'
+          },
+          date: {
+            start: '',
+            end: ''
+          },
+          techs: ['php', 'css', 'jquery']
+        },
+        {
+          slug: 'casasanta',
+          name: 'Casasanta Alimentos',
+          resume: 'Criação de site',
+          link: 'http://casasantaalimentos.com.br/',
+          company: {
+            name: 'Mídia Interativa',
+            link: 'https://www.midiainterativa.com.br'
+          },
+          date: {
+            start: '',
+            end: ''
+          },
+          techs: ['css', 'jquery']
+        },
+        {
+          slug: 'tectapetes',
+          name: 'Tectapetes',
+          resume: 'Criação de site',
+          link: 'http://www.tectapetes.com.br/',
+          company: {
+            name: 'Mídia Interativa',
+            link: 'https://www.midiainterativa.com.br'
+          },
+          date: {
+            start: '',
+            end: ''
+          },
+          techs: ['css', 'jquery']
+        }
+      ]
     }
   },
   head () {
